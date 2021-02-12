98d1cc3ad697258ae125324144e8626a6142c67f
Matthew Semeniuk <megaman22342@live.ca>
2021-02-12 19:52:16 +0000

    Added help command

---
+++ commands/HelpCommand.js
@@ -0,0 +1,29 @@
+const Command = require('./Command');
+const EmbedBuilder = require(`~/data/Builders/EmbedBuilder`);
+
+const options = {
+    names: ['help'],
+    expectedParameters: [],
+    global: true
+}
+
+class HelpCommand extends Command {
+    constructor(msg) {
+        super(msg, options);
+    }
+    async validate() {
+        super.validate();
+    }
+    async run() {
+        const embed = {
+            title: 'Help',
+            description: `View commands and manage your Pokemon at http://www.bluewave41.xyz/help.`
+        }
+        return EmbedBuilder.build(this.msg, embed);
+    }
+}
+
+module.exports = {
+    options: options,
+    class: HelpCommand
+}
