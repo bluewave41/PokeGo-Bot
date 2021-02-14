72f8fa560801e12acdb8846ec68295a14d3f5374
Matthew Semeniuk <megaman22342@live.ca>
2021-02-14 21:07:14 +0000

    Added shorthand for quit

--- commands/QuitCommand.js
+++ commands/QuitCommand.js
@@ -4,7 +4,7 @@ const Command = require('./Command');
 const User = require('~/knex/models/User');
 
 const options = {
-    names: ['quit'],
+    names: ['quit', 'q'],
     expectedParameters: [],
     global: true,
 }
